7548dcab639d58637e27ac387b683857d6d7dfcd
STaePark <STaeParkk@gmail.com>
2021-09-08 20:36:42 -0700

    Fixing repository

---
+++ 04-java-lecture/Day1.java
@@ -0,0 +1,12 @@
+public class Day1{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+
+        String a = "Very";
+        String b = "nice";
+        String c = "pog";
+        System.out.println(a + b + c);
+
+        System.out.printf("%s %s %s", a, b, c);
+    }
+}
